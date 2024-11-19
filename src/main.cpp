412dafe6ce97d17baef045d34edbaad33fa8a6bc
billbosh <joshuadonaldcarroll.com>
2024-11-19 20:00:27 +0000

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <cstddef>
+#include <unordered_map>
+
+struct midi_message {
+    int note;
+    int vel;
+};
 
 int main()
 {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
-    window.setFramerateLimit(144);
+    auto window = sf::RenderWindow({100u, 100u}, "CMake SFML Project");
+    window.setFramerateLimit(44100);
+    window.setKeyRepeatEnabled(false);
+    
+    std::unordered_map<char, int> keys = {
+        {'a', 64},
+        {'s', 65},
+        {'d', 66},
+        {'f', 67}
+    };
+    int octave = 0;
 
     while (window.isOpen())
     {
@@ -13,6 +29,35 @@ int main()
             {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.scancode == sf::Keyboard::Scancode::A) {
+                    // send midi on message of note: keys['a']
+                }
+                else if (event.key.scancode == sf::Keyboard::Scancode::S) {
+                    // send midi on message of note: keys['s']
+                } 
+                else if (event.key.scancode == sf::Keyboard::Scancode::D) {
+                    // send midi on message of note: keys['d']
+                }   
+                else if (event.key.scancode == sf::Keyboard::Scancode::F) {
+                    // send midi on message of note: keys['f']
+                }
+            }
+
+            if (event.type == sf::Event::KeyReleased) {
+                if (event.key.scancode == sf::Keyboard::Scancode::A) {
+                    // send midi off message of note: keys['a']
+                }
+                else if (event.key.scancode == sf::Keyboard::Scancode::S) {
+                    // send midi off message of note: keys['s']
+                } 
+                else if (event.key.scancode == sf::Keyboard::Scancode::D) {
+                    // send midi off message of note: keys['d']
+                }   
+                else if (event.key.scancode == sf::Keyboard::Scancode::F) {
+                    // send midi off message of note: keys['f']
+                }
+            }     
         }
 
         window.clear();

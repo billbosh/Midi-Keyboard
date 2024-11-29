e64c5067dc7048a129a7cfcaae1ea1373a43868c
billbosh <joshuadonaldcarroll.com>
2024-11-29 13:27:13 +0000

    Removed unnecessary arp switch statements

--- src/arpeggiator.cpp
+++ src/arpeggiator.cpp
@@ -7,40 +7,31 @@ void arpeggiate(libremidi::midi_out &midi) {}
 void arp_loop(libremidi::midi_out &midi,
               const std::unordered_map<sf::Keyboard::Scancode, int> &key_map,
               int &octave, sf::Event &event, bool &arpeggiate_on) {
-  if (arpeggiate_on) {
-    std::cout << "arpeggiate on" << std::endl;
-    if (event.type == sf::Event::KeyPressed) {
-      if (event.key.scancode == sf::Keyboard::Scancode::M) {
-        arpeggiate_on = !arpeggiate_on;
-      }
+  if (event.type == sf::Event::KeyPressed) {
+    // if key_map key pressed
+    if (key_map.contains(event.key.scancode)) {
+      midi.send_message(0x90, key_map.at(event.key.scancode) + 12 * octave,
+                        127);
     }
-  } else {
-    if (event.type == sf::Event::KeyPressed) {
-      // if key_map key pressed
-      if (key_map.contains(event.key.scancode)) {
-        midi.send_message(0x90, key_map.at(event.key.scancode) + 12 * octave,
-                          127);
-      }
-      // if octave up key pressed
-      else if (event.key.scancode == sf::Keyboard::Scancode::X && octave < 4) {
-        clear_arp_buffer();
-        ++octave;
-      }
-      // if octave down key pressed
-      else if (event.key.scancode == sf::Keyboard::Scancode::Z && octave > -5) {
-        clear_arp_buffer();
-        --octave;
-      } else if (event.key.scancode == sf::Keyboard::Scancode::M) {
-        clear_arp_buffer();
-        arpeggiate_on = !arpeggiate_on;
-        std::cout << "arpeggiate on" << std::endl;
-      }
-    } else if (event.type == sf::Event::KeyReleased) {
-      // if key_map key released
-      if (key_map.contains(event.key.scancode)) {
-        midi.send_message(0x80, key_map.at(event.key.scancode) + 12 * octave,
-                          127);
-      }
+    // if octave up key pressed
+    else if (event.key.scancode == sf::Keyboard::Scancode::X && octave < 4) {
+      clear_arp_buffer();
+      ++octave;
+    }
+    // if octave down key pressed
+    else if (event.key.scancode == sf::Keyboard::Scancode::Z && octave > -5) {
+      clear_arp_buffer();
+      --octave;
+    } else if (event.key.scancode == sf::Keyboard::Scancode::M) {
+      clear_arp_buffer();
+      arpeggiate_on = !arpeggiate_on;
+      std::cout << "arpeggiate off" << std::endl;
+    }
+  } else if (event.type == sf::Event::KeyReleased) {
+    // if key_map key released
+    if (key_map.contains(event.key.scancode)) {
+      midi.send_message(0x80, key_map.at(event.key.scancode) + 12 * octave,
+                        127);
     }
   }
 }
--- src/midi.cpp
+++ src/midi.cpp
@@ -12,40 +12,31 @@ void current_octave_notes_off(
 void midi_loop(libremidi::midi_out &midi,
                const std::unordered_map<sf::Keyboard::Scancode, int> &key_map,
                int &octave, sf::Event &event, bool &arpeggiate_on) {
-  if (arpeggiate_on) {
-    std::cout << "arpeggiate on" << std::endl;
-    if (event.type == sf::Event::KeyPressed) {
-      if (event.key.scancode == sf::Keyboard::Scancode::M) {
-        arpeggiate_on = !arpeggiate_on;
-      }
+  if (event.type == sf::Event::KeyPressed) {
+    // if key_map key pressed
+    if (key_map.contains(event.key.scancode)) {
+      midi.send_message(0x90, key_map.at(event.key.scancode) + 12 * octave,
+                        127);
     }
-  } else {
-    if (event.type == sf::Event::KeyPressed) {
-      // if key_map key pressed
-      if (key_map.contains(event.key.scancode)) {
-        midi.send_message(0x90, key_map.at(event.key.scancode) + 12 * octave,
-                          127);
-      }
-      // if octave up key pressed
-      else if (event.key.scancode == sf::Keyboard::Scancode::X && octave < 4) {
-        current_octave_notes_off(midi, key_map, octave);
-        ++octave;
-      }
-      // if octave down key pressed
-      else if (event.key.scancode == sf::Keyboard::Scancode::Z && octave > -5) {
-        current_octave_notes_off(midi, key_map, octave);
-        --octave;
-      } else if (event.key.scancode == sf::Keyboard::Scancode::M) {
-        current_octave_notes_off(midi, key_map, octave);
-        arpeggiate_on = !arpeggiate_on;
-        std::cout << "arpeggiate on" << std::endl;
-      }
-    } else if (event.type == sf::Event::KeyReleased) {
-      // if key_map key released
-      if (key_map.contains(event.key.scancode)) {
-        midi.send_message(0x80, key_map.at(event.key.scancode) + 12 * octave,
-                          127);
-      }
+    // if octave up key pressed
+    else if (event.key.scancode == sf::Keyboard::Scancode::X && octave < 4) {
+      current_octave_notes_off(midi, key_map, octave);
+      ++octave;
+    }
+    // if octave down key pressed
+    else if (event.key.scancode == sf::Keyboard::Scancode::Z && octave > -5) {
+      current_octave_notes_off(midi, key_map, octave);
+      --octave;
+    } else if (event.key.scancode == sf::Keyboard::Scancode::M) {
+      current_octave_notes_off(midi, key_map, octave);
+      arpeggiate_on = !arpeggiate_on;
+      std::cout << "arpeggiate on" << std::endl;
+    }
+  } else if (event.type == sf::Event::KeyReleased) {
+    // if key_map key released
+    if (key_map.contains(event.key.scancode)) {
+      midi.send_message(0x80, key_map.at(event.key.scancode) + 12 * octave,
+                        127);
     }
   }
 }
